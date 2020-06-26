abc13162547b30c80f4ba0312d6cdb7a355d7c85
DominicDr <dominikdrazek99@gmail.com>
2020-06-26 20:07:54 +0200

    Added fields and constructor to Card class

--- SuperheroClash/Card.cs
+++ SuperheroClash/Card.cs
@@ -2,11 +2,20 @@
 
 namespace SuperheroClash
 {
-	public class Card
-	{
-		public Card()
-		{
-		}
-	}
-}
+    public class Card
+    {
+        private int Rank;
+        private string Name;
+        private string Type;
+        private int State;
+
+        public Card(int rank, string name, string type, int state)
+        {
+            this.Rank = rank;
+            this.Name = name;
+            this.Type = type;
+            this.State = state;
+        }
+
+    }
 
