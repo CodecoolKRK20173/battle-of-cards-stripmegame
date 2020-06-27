2ee15adfcf84c3ec424ff929e8892474605b5028
DominicDr <dominikdrazek99@gmail.com>
2020-06-27 14:45:42 +0200

    Added class Deck.cs (not finishded yet)

---
+++ SuperheroClash/Deck.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SuperheroClash
+{
+	public class Deck
+	{
+		private List<Card> Cards;
+		public Deck(List<Card> cards)
+		{
+			this.Cards = cards;
+		}
+
+
+	}
+}
+
