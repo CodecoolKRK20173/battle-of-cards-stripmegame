565bd86f2d8efb09a415b164a7fdb42a9e45d7c8
Michal Mijal <mijal.michal@gmail.com>
2020-06-29 18:07:39 +0200

    added Hand methods and Player constructor

--- SuperheroClash/Hand.cs
+++ SuperheroClash/Hand.cs
@@ -1,11 +1,48 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace SuperheroClash
 {
     class Hand
     {
         public List<Card> CardsInHand { get; set; }
+        private int _StartingHand = 15;
 
+        public Hand()
+        {
+            List<Card> CardsInHand = new List<Card>();
+        }
+
+
+        public void GetHand()
+        {
+            CardsInHand = Deck.CreatingNewHand();
+        }
+
+        //public void GetHand(Deck deck)
+        //{
+        //    for (int i = 0; i < _StartingHand; i++)
+        //    {
+        //        CardsInHand.Add(deck.Deck[0]);
+        //        deck.Deck.Remove(deck.DEck[0]);
+        //    }
+        //}
+
+        public void AddCard(Card card)
+        {
+            CardsInHand.Add(card);
+        }
+
+        public void RemoveCard(Card card)
+        {
+            CardsInHand.Remove(card);
+        }
+
+        public int GetNrOfCardsInHand()
+        {
+            int _NrOfCards = CardsInHand.Count();
+            return _NrOfCards;
+        }
     }
 }
--- SuperheroClash/Player.cs
+++ SuperheroClash/Player.cs
@@ -1,6 +1,22 @@
-﻿namespace SuperheroClash
+﻿using System;
+using System.Collections.Generic;
+
+namespace SuperheroClash
 {
     class Player
     {
+        public string Name { get; set; }
+        public Hand Hand { get; set; }
+        public bool IsActive { get; set; }
+
+        public Player(string name, bool isActive)
+        {
+            Name = name;
+            Hand = new Hand();
+            IsActive = isActive;
+        }
+
+
+
     }
 }
