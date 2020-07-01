5b5d7d75bbd5741524d2e4c5684ba3ee064da298
Michal Mijal <mijal.michal@gmail.com>
2020-07-01 17:19:29 +0200

    Added PickStat method to Player

--- SuperheroClash/Player.cs
+++ SuperheroClash/Player.cs
@@ -9,12 +9,48 @@ namespace SuperheroClash
         public Hand Hand { get; set; }
         public bool IsActive { get; set; }
         public bool HasLost { get; set; }
+        public enum Stats
+        {
+            Rank = 1,
+            Power = 2,
+            Inteligence = 3,
+            Strength = 4
+        }
+
+        private List<int> _statsToCompare = new List<int>()
+        {
+            (int)Stats.Rank,
+            (int)Stats.Power,
+            (int)Stats.Inteligence,
+            (int)Stats.Strength
+        };
 
         public Player(string name, bool isActive)
         {
             Name = name;
             Hand = new Hand();
             IsActive = isActive;
+        }
+
+        public int PickStat()
+        { 
+            int inputStat = int.Parse(Console.ReadLine());
+            while (! _statsToCompare.Contains(inputStat))
+            {
+                try
+                {
+                    if (!_statsToCompare.Contains(inputStat)) throw new ArgumentException();
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Enter 1, 2, 3 or 4.");
+                }
+                catch (ArgumentException)
+                {
+                    Console.WriteLine("Enter 1, 2, 3 or 4.");
+                }
+            }
+            return inputStat;
         }
 
 
