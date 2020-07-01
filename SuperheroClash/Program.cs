5fb67929cf1275186cba8e0a4ff439353ab873f7
DominicDr <dominikdrazek99@gmail.com>
2020-07-01 21:00:45 +0200

    Creating Main loop in class Program and added class Options

--- SuperheroClash/Program.cs
+++ SuperheroClash/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Security.Cryptography;
 
 namespace SuperheroClash
@@ -7,14 +8,38 @@ namespace SuperheroClash
     {
         static void Main(string[] args)
         {
-            var CardsOperation = new CardsDAO(@"D:\VisualStudio\CodeCool\SuperheroClash\SuperheroClash\Cards.csv");
-            var HeroArray = CardsOperation.SplittingFile();
-            var NewCards = CardsOperation.CreatingNewCards(HeroArray);
-            Deck NewDeck = new Deck(NewCards);
-            var NewHand = NewDeck.CreatingNewHand();
-            foreach (var card in NewHand)
-                card.DisplayingCard();
-            View.MakeCard();
+            var Menu = new Options();
+            Menu.MainMenuOptions();
+            
+            bool IsPlaying = true; 
+
+            while(IsPlaying)
+            {
+                Console.WriteLine("Choose an option by enter a number");
+                var Choice = Convert.ToInt16(Console.ReadLine());
+
+                if (Choice == 1)
+                {
+                    Console.WriteLine("Playing with friend");
+                    IsPlaying = false;
+                }
+
+                else if (Choice == 2)
+                {
+                    Console.WriteLine("Playing with computer");
+                    IsPlaying = false;
+                }
+                    
+                else if (Choice == 3)
+                {
+                    Console.WriteLine("Reading rules");
+                    IsPlaying = false;
+                }
+
+                else if (Choice == 4)
+                    IsPlaying = false;
+
+            }
         }
     }
 }
