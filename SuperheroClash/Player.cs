85c75681e36b5d593b490432158f86ff822fa401
Michal Mijal <mijal.michal@gmail.com>
2020-07-01 14:25:52 +0200

    Added SetHasLost to Player class

--- SuperheroClash/Player.cs
+++ SuperheroClash/Player.cs
@@ -8,12 +8,22 @@ namespace SuperheroClash
         public string Name { get; set; }
         public Hand Hand { get; set; }
         public bool IsActive { get; set; }
+        public bool HasLost { get; set; }
 
         public Player(string name, bool isActive)
         {
             Name = name;
             Hand = new Hand();
             IsActive = isActive;
+        }
+
+
+        public void SetHasLost()
+        {
+            if(Hand.IsEmpty == true)
+            {
+                HasLost = true;
+            }
         }
 
 
