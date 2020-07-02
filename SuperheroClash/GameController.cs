5aab80e1a027cc0dfdb1c839e40097b2daa194db
Ricardo Reid <Ricardo.Reid@ipaper.com>
2020-07-02 13:42:45 +0200

    Implemented GameController

--- SuperheroClash/GameController.cs
+++ SuperheroClash/GameController.cs
@@ -2,6 +2,50 @@ namespace SuperheroClash
 {
     public class GameController
     {
-        
+        Player Player1;
+        Player Player2;
+
+        CardsDAO myCards;
+
+        Deck myDeck;
+        CardComparer myComparer;
+
+
+        public GameController( string filePath )
+        {
+            myCards = new CardsDAO( filePath );
+            myDeck  = new Deck( myCards.CreatingNewCards(myCards.SplittingFile() ) );
+            Player1.Hand.CardsInHand = myDeck.CreatingNewHand();
+            Player2.Hand.CardsInHand = myDeck.CreatingNewHand();
+            Player1.HasLost = false;
+            Player2.HasLost = false;
+
+            Player1.IsActive = true; // maybe a new method to handle this?
+        }
+
+        public void GamePlay()
+        {
+            while ( !Player1.HasLost && !Player2.HasLost )
+            {
+                // get players card with stats to compare
+                myComparer.Compare( ref Player1, ref Player2 );
+                    // compare cards
+                    // add card to player hand with greater stats... remove other player hand
+                        // if equal... get more cards from players
+                // check number of cards in players' hand
+                if ( Player1.Hand.CardsInHand.Count == 30 )
+                {
+                    Player2.HasLost = true;
+                }
+                else if ( Player2.Hand.CardsInHand.Count == 30 )
+                {
+                    Player1.HasLost = true;
+                }
+                // assign winner and loser to correct players
+            }
+
+
+            
+        }
     }
 }
--- SuperheroClash/Player.cs
+++ SuperheroClash/Player.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace SuperheroClash
 {
-    class Player
+    public class Player
     {
         public string Name { get; set; }
         public Hand Hand { get; set; }
