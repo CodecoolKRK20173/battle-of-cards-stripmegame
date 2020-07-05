739ea2d3d4c9df885c7cce98d904197b3fc17db5
ricoreid <reid.ricardo@gmail.com>
2020-07-05 12:57:47 +0200

    Added Game Rules

--- SuperheroClash/View.cs
+++ SuperheroClash/View.cs
@@ -26,6 +26,19 @@ namespace SuperheroClash
             Console.WriteLine(player2.Hand.CardsInHand[_TOPCARD]);
         }
 
+        public void GameRules()
+        {
+            Console.WriteLine("********** GAME RULES **********");
+            Console.WriteLine("1.   Each player gets a total of 15 cards");
+            Console.WriteLine("2.   All cards have different features/capabilities that are ranked");
+            Console.WriteLine("3.   The active player will choose a feature of the current card to compare with the second player's current card");
+            Console.WriteLine("4.   The player with the highest ranking card wins");
+            Console.WriteLine("5.   If there is a tie, both players compare the next card in their hands and repeat the above steps");
+            Console.WriteLine("6.   The player who wins takes all the cards on the board");
+            Console.WriteLine("7.   This process is repeated until one play has no cards left");
+
+        }
+
     }
 }
 
